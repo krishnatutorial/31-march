75fe28e37090e942d482194be2588506883f1c7f
krishnatutorial <krishnakjp963@gmail.com>
2024-03-31 06:49:17 +0000

    code giving error

---
+++ src/components/userInput/styledComponents.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const UserInputItem = styled.li`
+  list-style-type: none;
+`
+
+export const UserEnteredDetails = styled.p`
+  font-weight: bold;
+  color: #0f172a;
+  font-size: 18px;
+  font-family: 'Roboto';
+`
